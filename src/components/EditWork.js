51198f4799fa9ef0c68bd03f8cb1b1c2a2ac4a59
Gepoverlow <gepeoverload@gmail.com>
2022-03-22 14:00:49 +0100

    succesfully implemented editing component for experience

--- src/components/EditWork.js
+++ src/components/EditWork.js
@@ -8,17 +8,38 @@ class EditWork extends Component {
     return (
       <div className="container-work">
         <div className="work-company">
-          <input value={this.props.companyEdit}></input>
+          <input
+            onChange={this.props.handleChangeCompany}
+            defaultValue={this.props.companyEdit}
+          ></input>
+          <span
+            className="material-icons-outlined"
+            onClick={this.props.handleDelete}
+          >
+            clear
+          </span>
         </div>
         <div className="work-role-date">
-          <input value={this.props.roleEdit}></input>
+          <input
+            onChange={this.props.handleChangeRole}
+            defaultValue={this.props.roleEdit}
+          ></input>
           <div className="work-date-edit">
-            <input value={this.props.dateFromEdit}></input>
-            <input value={this.props.dateToEdit}></input>
+            <input
+              onChange={this.props.handleChangeFrom}
+              defaultValue={this.props.dateFromEdit}
+            ></input>
+            <input
+              onChange={this.props.handleChangeTo}
+              defaultValue={this.props.dateToEdit}
+            ></input>
           </div>
         </div>
         <div className="work-detals">
-          <input value={this.props.detailsEdit}></input>
+          <input
+            onChange={this.props.handleChangeDetails}
+            defaultValue={this.props.detailsEdit}
+          ></input>
         </div>
       </div>
     );
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -56,6 +56,85 @@ class Experience extends Component {
     }));
   }
 
+  //
+  handleChangeCompany(index, e) {
+    const array = [...this.state.works];
+    const itemIndex = array.findIndex((work) => {
+      return work.id === index;
+    });
+    const work = { ...array[itemIndex] };
+    work.company = e.target.value;
+
+    array[itemIndex] = work;
+
+    this.setState({ works: array });
+  }
+
+  handleChangeRole(index, e) {
+    const array = [...this.state.works];
+    const itemIndex = array.findIndex((work) => {
+      return work.id === index;
+    });
+    const work = { ...array[itemIndex] };
+    work.role = e.target.value;
+
+    array[itemIndex] = work;
+
+    this.setState({ works: array });
+  }
+
+  handleChangeFrom(index, e) {
+    const array = [...this.state.works];
+    const itemIndex = array.findIndex((work) => {
+      return work.id === index;
+    });
+    const work = { ...array[itemIndex] };
+    work.dateFrom = e.target.value;
+
+    array[itemIndex] = work;
+
+    this.setState({ works: array });
+  }
+
+  handleChangeTo(index, e) {
+    const array = [...this.state.works];
+    const itemIndex = array.findIndex((work) => {
+      return work.id === index;
+    });
+    const work = { ...array[itemIndex] };
+    work.dateTo = e.target.value;
+
+    array[itemIndex] = work;
+
+    this.setState({ works: array });
+  }
+
+  handleChangeDetails(index, e) {
+    const array = [...this.state.works];
+    const itemIndex = array.findIndex((work) => {
+      return work.id === index;
+    });
+    const work = { ...array[itemIndex] };
+    work.details = e.target.value;
+
+    array[itemIndex] = work;
+
+    this.setState({ works: array });
+  }
+  //
+
+  handleDeleteWork(index) {
+    const array = [...this.state.works];
+    const itemIndex = array.findIndex((work) => {
+      return work.id === index;
+    });
+
+    if (itemIndex !== -1) {
+      array.splice(itemIndex, 1);
+      this.setState({ works: array });
+    }
+  }
+
   render() {
     return (
       <div className="container-experience">
@@ -83,6 +162,16 @@ class Experience extends Component {
                   dateFromEdit={work.dateFrom}
                   dateToEdit={work.dateTo}
                   detailsEdit={work.details}
+                  handleChangeCompany={(e) =>
+                    this.handleChangeCompany(work.id, e)
+                  }
+                  handleChangeRole={(e) => this.handleChangeRole(work.id, e)}
+                  handleChangeFrom={(e) => this.handleChangeFrom(work.id, e)}
+                  handleChangeTo={(e) => this.handleChangeTo(work.id, e)}
+                  handleChangeDetails={(e) =>
+                    this.handleChangeDetails(work.id, e)
+                  }
+                  handleDelete={() => this.handleDeleteWork(work.id)}
                 />
               );
             })
